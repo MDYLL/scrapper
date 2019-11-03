aa7edd820e02ae84ee2472023f10b7f2a2116900
Dmitri Martynov <mdylll@mail.ru>
2019-11-03 11:58:27 +0300

    Update scrapper.py

--- scrapper.py
+++ scrapper.py
@@ -2,7 +2,6 @@
 main functions for scrapping
 """
 
-
 import datetime
 import itertools
 import os
@@ -36,13 +35,16 @@ def init_iata(db_file, schedule_url):
 
 def scrap(trip, schedule, round_trip, url):
     """
-    scrapping URL and print result (possible flights)
+    scrapping URL and return result (possible flights)
     :param trip: trip information
     :param schedule:
     :param round_trip: true if round else false
     :param url: url for parsing tickets
-    :return: nothing
+    :return: reservation - message and list of the flights
     """
+    reservations = list()
+    reservations.append("message")
+    reservations.append(list())
     if not round_trip:  # one way trip
         for day in schedule[0]:
             date_flight = datetime.date(
@@ -51,32 +53,34 @@ def scrap(trip, schedule, round_trip, url):
             data_request = operations.create_data_request(current_trip)
             tree_xml = operations.get_xml(url, data_request)
             if tree_xml is None:
-                print("No connection to URL")
-                return
+                reservations[0] = "No connection to URL"
+                return reservations
             flight_list = operations.parse_xml(current_trip, tree_xml)
             if len(flight_list[0]) != 0:
-                print(date_flight)
+                reservations[0] = date_flight
                 for flight in flight_list[0]:
-                    print(flight)
-            return
+                    reservations[1].append(flight)
+            return reservations
     # round trip
     if len(schedule[1]) == 0:
-        print("You could by ticket from " + trip[0] + " to " +
-              trip[1] + " for next days:")
+        reservations[0] = "You could by ticket from " + trip[0] + " to " + \
+                          trip[1] + " for next days:"
         for day in schedule[0]:
             date_flight = datetime.date(
                 *list(map(int, trip[2].split('-')))) + datetime.timedelta(day)
-            print(date_flight)
-        print("But there are no flights back for "
-              + trip[3] + " and 2 next days")
-        return
+            reservations[0] += "\n"
+            reservations[0] += date_flight
+        reservations[0] += "But there are no flights back for " \
+                           + trip[3] + " and 2 next days"
+        return reservations
 
     for day in schedule[0]:
         for day_back in schedule[1]:
             date_flight = datetime.date(
                 *list(map(int, trip[2].split('-')))) + datetime.timedelta(day)
             date_flight_back = datetime.date(
-                *list(map(int, trip[3].split('-')))) + datetime.timedelta(day_back)
+                *list(map(int, trip[3].split('-')))) + \
+                               datetime.timedelta(day_back)
             if date_flight > date_flight_back:
                 continue
             current_trip = TimeTable(trip[0], trip[1], str(date_flight),
@@ -84,8 +88,8 @@ def scrap(trip, schedule, round_trip, url):
             data_request = operations.create_data_request(current_trip)
             tree_xml = operations.get_xml(url, data_request)
             if tree_xml is None:
-                print("No connection to URL")
-                return
+                reservations[0] = "No connection to URL"
+                return reservations
             flight_list = operations.parse_xml(current_trip, tree_xml)
             if len(flight_list[0]) * len(flight_list[1]) > 0:
                 flight_combination = list(map(list, itertools.product(
@@ -96,14 +100,15 @@ def scrap(trip, schedule, round_trip, url):
                 operations.add_total_price(flight_combination)
                 flight_combination.sort(key=lambda x: x[2])
                 if len(flight_combination) > 0:
-                    print("From " + trip[0] + " to " + trip[1] + " " +
-                          str(date_flight))
-                    print("From " + trip[1] + " to " + trip[0] + " " +
-                          str(date_flight_back))
+                    reservations[0] = "From " + trip[0] + " to " + trip[1] \
+                                      + " " + str(date_flight)
+                    reservations[0] += "\n"
+                    reservations[0] += "From " + trip[0] + " to " + trip[1] \
+                                       + " " + str(date_flight)
                     for flights in flight_combination:
                         for flight in flights:
-                            print(flight)
-                    return
+                            reservations[1].append(flight)
+                    return reservations
 
-    print("No flights for your dates")
-    return
+    reservations[0] = "No flights for your dates"
+    return reservations
